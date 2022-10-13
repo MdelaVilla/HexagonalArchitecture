a3efa01a4efa46293f1f5666fd5d29793f105a71
Manuel <Manuel@ETP131-manuel-villa-dti>
2022-10-13 13:29:40 +0200

    Actualizados los recursos para que pueda hacer una carga inicial de datos de modo que la consulta al endpoint devuelva un valor.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Book VALUES (1,'Don Quijote de la Mancha');
+INSERT INTO Book VALUES (2,'Fortunata y Jacinta');
+INSERT INTO Book VALUES (3,'Othello');
+INSERT INTO Book VALUES (4,'El rey Lear');
